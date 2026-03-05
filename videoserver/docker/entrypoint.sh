4c78ecaac7d6a75214f350eba56fa6dc889dd958
Noman <noman.ali.shaukat@gmail.com>
2026-03-05 15:12:47 +0100

    chore: upgrade janus version to 1.4.0

---
+++ videoserver/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+JANUS_CFG="/usr/local/etc/janus/janus.jcfg"
+
+if [ -n "$NAT_IP" ]; then
+	sed -i "s|#nat_1_1_mapping = .*|nat_1_1_mapping = \"$NAT_IP\"|" "$JANUS_CFG"
+	echo "nat_1_1_mapping set to: $NAT_IP"
+else
+	echo "Warning: NAT_IP is not set, nat_1_1_mapping will not be configured"
+fi
+
+exec /usr/local/bin/janus
